12285ff711e08e969b54639ac798b14acfa6b6f6
Andrew Grosner <andrew.grosner@raizlabs.com>
2014-09-18 13:21:50 -0400

    made modelview implement model so we can load them within the database manager. it will throw exceptions for any modification methods

--- src/main/java/com/grosner/dbflow/structure/BaseModelView.java
+++ src/main/java/com/grosner/dbflow/structure/BaseModelView.java
@@ -1,5 +1,9 @@
 package com.grosner.dbflow.structure;
 
+import android.database.Cursor;
+
+import com.grosner.dbflow.config.FlowManager;
+import com.grosner.dbflow.sql.SqlUtils;
 import com.grosner.dbflow.sql.Where;
 
 /**
@@ -7,7 +11,17 @@ import com.grosner.dbflow.sql.Where;
  * Contributors: { }
  * Description: Provides a base implementation for a {@link com.grosner.dbflow.structure.ModelView}.
  */
-public abstract class BaseModelView<ModelClass extends Model> implements ModelView<ModelClass> {
+public abstract class BaseModelView<ModelClass extends Model> implements ModelView<ModelClass>, Model {
+
+    /**
+     * Gets thrown when an operation is not valid for the SQL {@link com.grosner.dbflow.structure.ModelView}
+     */
+    private class InvalidSqlViewOperationException extends RuntimeException{
+
+        private InvalidSqlViewOperationException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
 
     @Override
     public abstract Where<ModelClass> getWhere();
@@ -15,5 +29,30 @@ public abstract class BaseModelView<ModelClass extends Model> implements ModelVi
     @Override
     public String getName() {
         return getClass().getSimpleName();
+    }
+
+    @Override
+    public void save(boolean async) {
+        throw new InvalidSqlViewOperationException("View " + getName() + " is not saveable");
+    }
+
+    @Override
+    public void delete(boolean async) {
+        throw new InvalidSqlViewOperationException("View " + getName() + " is not deleteable");
+    }
+
+    @Override
+    public void update(boolean async) {
+        throw new InvalidSqlViewOperationException("View " + getName() + " is not updateable");
+    }
+
+    @Override
+    public void load(Cursor cursor) {
+        SqlUtils.loadFromCursor(FlowManager.getInstance(), this, cursor);
+    }
+
+    @Override
+    public boolean exists() {
+        return SqlUtils.exists(FlowManager.getInstance(), this);
     }
 }
