1be1011cd9c9c34de722c12c1d3fce2f99b3a1b8
Andrew Grosner <andrew.grosner@raizlabs.com>
2014-09-18 22:56:57 -0400

    updated more comments

--- src/main/java/com/grosner/dbflow/sql/builder/QueryBuilder.java
+++ src/main/java/com/grosner/dbflow/sql/builder/QueryBuilder.java
@@ -14,38 +14,84 @@ import java.util.List;
  */
 public class QueryBuilder<QueryClass extends QueryBuilder> implements Query {
 
+    /**
+     * This query is backed by a {@link java.lang.StringBuilder}
+     */
     protected StringBuilder mQuery = new StringBuilder();
 
+    /**
+     * Appends a string to this query
+     * @param string The string to append
+     * @return
+     */
     public QueryClass append(String string) {
         mQuery.append(string);
         return castThis();
     }
 
+    /**
+     * Appends a space to this query
+     * @return
+     */
     public QueryClass appendSpace() {
         return append(" ");
     }
 
+    /**
+     * Appends the string with spaces on the front and end of the string
+     * @param string The string to append
+     * @return
+     */
     @SuppressWarnings("unchecked")
     public QueryClass appendSpaceSeparated(String string) {
         return (QueryClass) appendSpace().append(string).appendSpace();
     }
 
+    /**
+     * Appends an {@link com.grosner.dbflow.structure.SQLiteType} to this query based on the class
+     * passed in.
+     * @param type The Class to look up from {@link com.grosner.dbflow.structure.SQLiteType}
+     * @return
+     */
     public QueryClass appendType(Class type) {
         return appendSQLiteType(SQLiteType.get(type));
     }
 
+    /**
+     * Appends the {@link com.grosner.dbflow.structure.SQLiteType} to this query
+     * @param sqLiteType The {@link com.grosner.dbflow.structure.SQLiteType} to append
+     * @return
+     */
     public QueryClass appendSQLiteType(SQLiteType sqLiteType) {
         return append(sqLiteType.name());
     }
 
-    public QueryClass appendArray(Object[] objects) {
+    /**
+     * Appends an array of these objects by joining them with a comma with
+     * {@link android.text.TextUtils#join(CharSequence, Object[])}
+     * @param objects The array of objects to pass in
+     * @return
+     */
+    public QueryClass appendArray(Object... objects) {
         return append(TextUtils.join(", ", objects));
     }
 
+    /**
+     * Appends a list of objects by joining them with a comma with
+     * {@link android.text.TextUtils#join(CharSequence, Object[])}
+     * @param objects The list of objects to pass in
+     * @return
+     */
     public QueryClass appendList(List<?> objects) {
         return append(TextUtils.join(", ", objects));
     }
 
+    /**
+     * Appends a value only if it's not {@link android.text.TextUtils#isEmpty(CharSequence)}
+     * @param name The name of the qualifier
+     * @param value The value to append after the name
+     * @return
+     */
     public QueryClass appendQualifier(String name, String value) {
         if (!TextUtils.isEmpty(value)) {
             append(name).appendSpaceSeparated(value);
@@ -53,6 +99,10 @@ public class QueryBuilder<QueryClass extends QueryBuilder> implements Query {
         return castThis();
     }
 
+    /**
+     * Casts the current object to the {@link QueryClass}
+     * @return
+     */
     @SuppressWarnings("unchecked")
     protected QueryClass castThis() {
         return (QueryClass) this;
