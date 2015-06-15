5272d98f1473d30c2238c3d936f840c4cdd14130
Jason Pearson <jason.d.pearson@gmail.com>
2015-06-15 00:19:20 -0400

    add getWritableDatabase call to BaseDatabaseDefinition

--- DBFlow/src/main/java/com/raizlabs/android/dbflow/config/BaseDatabaseDefinition.java
+++ DBFlow/src/main/java/com/raizlabs/android/dbflow/config/BaseDatabaseDefinition.java
@@ -217,6 +217,7 @@ public abstract class BaseDatabaseDefinition {
             context.deleteDatabase(getDatabaseFileName());
             flowSQLiteOpenHelper = new FlowSQLiteOpenHelper(this, internalHelperListener);
             isResetting = false;
+            flowSQLiteOpenHelper.getWritableDatabase();
         }
     }
 
