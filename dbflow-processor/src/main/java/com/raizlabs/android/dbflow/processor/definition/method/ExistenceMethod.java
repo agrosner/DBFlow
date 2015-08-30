31a2ce32b8f8b4f29b4599c47051bfe26f5f0638
fuzzagrosner <andrew@fuzzproductions.com>
2015-08-30 10:06:39 -0400

    fix existence call

--- dbflow-processor/src/main/java/com/raizlabs/android/dbflow/processor/definition/method/ExistenceMethod.java
+++ dbflow-processor/src/main/java/com/raizlabs/android/dbflow/processor/definition/method/ExistenceMethod.java
@@ -50,7 +50,7 @@ public class ExistenceMethod implements MethodDefinition {
                 selectBuilder.add("$L.$L", tableDefinition.getPropertyClassName(), columnDefinition.columnName);
             }
             methodBuilder.addStatement("return new $T($L).from($T.class).where(getPrimaryConditionClause($L)).hasData()",
-                    ClassNames.SELECT, selectBuilder.build(), tableDefinition.elementClassName, PARAM_MODEL);
+                    ClassNames.SELECT, selectBuilder.build(), tableDefinition.elementClassName, ModelUtils.getVariable(isModelContainerAdapter));
         }
 
         return methodBuilder.build();
