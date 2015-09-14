778e27f519180d770d07281b0e8754217b85cf9b
fuzzagrosner <andrew@fuzzproductions.com>
2015-09-13 22:29:48 -0400

    added complex foreign key example

--- dbflow/src/androidTest/java/com/raizlabs/android/dbflow/test/structure/foreignkey/ForeignModelNoReferences.java
+++ dbflow/src/androidTest/java/com/raizlabs/android/dbflow/test/structure/foreignkey/ForeignModelNoReferences.java
@@ -2,6 +2,7 @@ package com.raizlabs.android.dbflow.test.structure.foreignkey;
 
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.ForeignKey;
+import com.raizlabs.android.dbflow.annotation.ModelContainer;
 import com.raizlabs.android.dbflow.annotation.Table;
 import com.raizlabs.android.dbflow.test.TestDatabase;
 import com.raizlabs.android.dbflow.test.structure.TestModel1;
@@ -9,6 +10,7 @@ import com.raizlabs.android.dbflow.test.structure.TestModel1;
 /**
  * Description:
  */
+@ModelContainer
 @Table(database = TestDatabase.class)
 public class ForeignModelNoReferences extends TestModel1 {
 
@@ -19,4 +21,8 @@ public class ForeignModelNoReferences extends TestModel1 {
     @Column
     @ForeignKey(tableClass = ForeignParentModel.class)
     String parentName;
+
+    @Column
+    @ForeignKey
+    ForeignParentModel2 parentModel2;
 }
