6439ca584e576c01c6543385d791b7f767003498
fuzzagrosner <andrew@fuzzproductions.com>
2015-11-26 22:21:24 -0500

    decreased adapter size by not writing save/update/insert methods if onetomany exists

--- dbflow-processor/src/main/java/com/raizlabs/android/dbflow/processor/definition/method/OneToManySaveMethod.java
+++ dbflow-processor/src/main/java/com/raizlabs/android/dbflow/processor/definition/method/OneToManySaveMethod.java
@@ -28,27 +28,31 @@ public class OneToManySaveMethod implements MethodDefinition {
 
     @Override
     public MethodSpec getMethodSpec() {
-        CodeBlock.Builder code = CodeBlock.builder();
-        for (OneToManyDefinition oneToManyDefinition : tableDefinition.oneToManyDefinitions) {
-            switch (methodName) {
-                case METHOD_SAVE:
-                    oneToManyDefinition.writeSave(code);
-                    break;
-                case METHOD_UPDATE:
-                    oneToManyDefinition.writeUpdate(code);
-                    break;
-                case METHOD_INSERT:
-                    oneToManyDefinition.writeInsert(code);
-                    break;
+        if (!tableDefinition.oneToManyDefinitions.isEmpty()) {
+            CodeBlock.Builder code = CodeBlock.builder();
+            for (OneToManyDefinition oneToManyDefinition : tableDefinition.oneToManyDefinitions) {
+                switch (methodName) {
+                    case METHOD_SAVE:
+                        oneToManyDefinition.writeSave(code);
+                        break;
+                    case METHOD_UPDATE:
+                        oneToManyDefinition.writeUpdate(code);
+                        break;
+                    case METHOD_INSERT:
+                        oneToManyDefinition.writeInsert(code);
+                        break;
+                }
             }
-        }
-        code.addStatement("super.$L($L)", methodName, ModelUtils.getVariable(isModelContainerAdapter));
+            code.addStatement("super.$L($L)", methodName, ModelUtils.getVariable(isModelContainerAdapter));
 
-        return MethodSpec.methodBuilder(methodName)
-                .addAnnotation(Override.class)
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addParameter(tableDefinition.elementClassName, ModelUtils.getVariable(isModelContainerAdapter))
-                .addCode(code.build())
-                .build();
+            return MethodSpec.methodBuilder(methodName)
+                    .addAnnotation(Override.class)
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                    .addParameter(tableDefinition.elementClassName, ModelUtils.getVariable(isModelContainerAdapter))
+                    .addCode(code.build())
+                    .build();
+        } else {
+            return null;
+        }
     }
 }
