7896f0e339de157434384d01507b6d018ebb6c47
fuzzagrosner <andrew@fuzzproductions.com>
2016-01-16 15:35:21 -0500

    hook into calls

--- dbflow/src/main/java/com/raizlabs/android/dbflow/sql/language/BaseModelQueriable.java
+++ dbflow/src/main/java/com/raizlabs/android/dbflow/sql/language/BaseModelQueriable.java
@@ -3,6 +3,7 @@ package com.raizlabs.android.dbflow.sql.language;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
 
+import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.list.FlowCursorList;
 import com.raizlabs.android.dbflow.list.FlowQueryList;
 import com.raizlabs.android.dbflow.runtime.TransactionManager;
@@ -13,7 +14,6 @@ import com.raizlabs.android.dbflow.sql.queriable.ModelQueriable;
 import com.raizlabs.android.dbflow.structure.BaseQueryModel;
 import com.raizlabs.android.dbflow.structure.InstanceAdapter;
 import com.raizlabs.android.dbflow.structure.Model;
-import com.raizlabs.android.dbflow.structure.RetrievalAdapter;
 import com.raizlabs.android.dbflow.structure.container.ModelContainer;
 
 import java.util.List;
@@ -24,6 +24,7 @@ import java.util.List;
 public abstract class BaseModelQueriable<ModelClass extends Model> implements ModelQueriable<ModelClass>, Query {
 
     private final Class<ModelClass> table;
+    private final InstanceAdapter<?, ModelClass> retrievalAdapter;
 
     /**
      * Constructs new instance of this class and is meant for subclasses only.
@@ -32,16 +33,18 @@ public abstract class BaseModelQueriable<ModelClass extends Model> implements Mo
      */
     protected BaseModelQueriable(Class<ModelClass> table) {
         this.table = table;
+        //noinspection unchecked
+        retrievalAdapter = FlowManager.getInstanceAdapter(table);
     }
 
     @Override
     public List<ModelClass> queryList() {
-        return SqlUtils.queryList(table, getQuery());
+        return retrievalAdapter.getListModelLoader().load(getQuery());
     }
 
     @Override
     public ModelClass querySingle() {
-        return SqlUtils.querySingle(table, getQuery());
+        return retrievalAdapter.getSingleModelLoader().load(getQuery());
     }
 
     @Override
@@ -71,12 +74,12 @@ public abstract class BaseModelQueriable<ModelClass extends Model> implements Mo
 
     @Override
     public <QueryClass extends BaseQueryModel> List<QueryClass> queryCustomList(Class<QueryClass> queryModelClass) {
-        return SqlUtils.queryList(queryModelClass, getQuery());
+        return FlowManager.getQueryModelAdapter(queryModelClass).getListModelLoader().load(getQuery());
     }
 
     @Override
     public <QueryClass extends BaseQueryModel> QueryClass queryCustomSingle(Class<QueryClass> queryModelClass) {
-        return SqlUtils.querySingle(queryModelClass, getQuery());
+        return FlowManager.getQueryModelAdapter(queryModelClass).getSingleModelLoader().load(getQuery());
     }
 
     @Override
