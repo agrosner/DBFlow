6dfd5d6d61dbdc428954dcb8076e4d7a5b762f7c
James H. Hill <hilljh82@gmail.com>
2016-02-23 11:25:40 -0500

    Removed test files

--- dbflow/src/androidTest/java/com/raizlabs/android/dbflow/test/structure/externalforeignkey/TestDatabase1.java
+++
@@ -1,7 +0,0 @@
-package com.raizlabs.android.dbflow.test.structure.externalforeignkey;
-
-import com.raizlabs.android.dbflow.annotation.Database;
-
-@Database(name="externalforeignkey_database1", version=1)
-public class TestDatabase1 {
-}
