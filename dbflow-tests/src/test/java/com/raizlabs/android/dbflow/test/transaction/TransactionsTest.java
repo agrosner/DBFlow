7d9fec4142347e079378b75686c14853640cd53c
fuzzagrosner <andrew@fuzzproductions.com>
2016-05-03 14:22:32 -0400

    run callbacks on same thread. adding in test for testing time and printing it of a bunch of models saving.

--- dbflow-tests/src/test/java/com/raizlabs/android/dbflow/test/transaction/TransactionsTest.java
+++ dbflow-tests/src/test/java/com/raizlabs/android/dbflow/test/transaction/TransactionsTest.java
@@ -5,7 +5,9 @@ import android.support.annotation.NonNull;
 import com.raizlabs.android.dbflow.config.DatabaseDefinition;
 import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.language.CursorResult;
+import com.raizlabs.android.dbflow.sql.language.Delete;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.structure.Model;
 import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
 import com.raizlabs.android.dbflow.structure.database.transaction.ITransaction;
 import com.raizlabs.android.dbflow.structure.database.transaction.ProcessModelTransaction;
@@ -15,6 +17,8 @@ import com.raizlabs.android.dbflow.test.FlowTestCase;
 import com.raizlabs.android.dbflow.test.TestDatabase;
 import com.raizlabs.android.dbflow.test.structure.TestModel1;
 import com.raizlabs.android.dbflow.test.structure.TestModel1_Table;
+import com.raizlabs.android.dbflow.test.structure.TestModel2;
+import com.raizlabs.android.dbflow.test.utils.GenerationUtils;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -80,17 +84,17 @@ public class TransactionsTest extends FlowTestCase {
         final AtomicBoolean processCalled = new AtomicBoolean(false);
         final AtomicInteger modelProcessedCount = new AtomicInteger(0);
         ProcessModelTransaction<TestModel1> processModelTransaction =
-                new ProcessModelTransaction.Builder<>(new ProcessModelTransaction.ProcessModel<TestModel1>() {
-                    @Override
-                    public void processModel(TestModel1 model) {
-                        processCalled.set(true);
-                    }
-                }).processListener(new ProcessModelTransaction.OnModelProcessListener<TestModel1>() {
-                    @Override
-                    public void onModelProcessed(long current, long total, TestModel1 modifiedModel) {
-                        modelProcessedCount.incrementAndGet();
-                    }
-                }).addAll(testModel1List).build();
+            new ProcessModelTransaction.Builder<>(new ProcessModelTransaction.ProcessModel<TestModel1>() {
+                @Override
+                public void processModel(TestModel1 model) {
+                    processCalled.set(true);
+                }
+            }).processListener(new ProcessModelTransaction.OnModelProcessListener<TestModel1>() {
+                @Override
+                public void onModelProcessed(long current, long total, TestModel1 modifiedModel) {
+                    modelProcessedCount.incrementAndGet();
+                }
+            }).addAll(testModel1List).build();
         Transaction transaction = new Transaction.Builder(processModelTransaction, database).build();
         new MockTransaction(transaction, database).execute();
 
@@ -116,9 +120,9 @@ public class TransactionsTest extends FlowTestCase {
 
         final AtomicBoolean called = new AtomicBoolean(false);
         QueryTransaction<TestModel1> queryTransaction = new QueryTransaction.Builder<>(
-                SQLite.select()
-                        .from(TestModel1.class)
-                        .where(TestModel1_Table.name.like("%test1"))
+            SQLite.select()
+                .from(TestModel1.class)
+                .where(TestModel1_Table.name.like("%test1"))
         ).queryResult(new QueryTransaction.QueryResultCallback<TestModel1>() {
             @Override
             public void onQueryResult(QueryTransaction transaction, @NonNull CursorResult<TestModel1> tResult) {
@@ -133,6 +137,39 @@ public class TransactionsTest extends FlowTestCase {
         new MockTransaction(transaction, database).execute();
 
         assertTrue(called.get());
+    }
+
+    @Test
+    public void test_bunchaModels() {
+        Delete.tables(TestModel1.class, TestModel2.class);
+
+        List<Model> modelList = new ArrayList<>();
+        modelList.addAll(GenerationUtils.generateRandomModels(TestModel1.class, 50));
+        modelList.addAll(GenerationUtils.generateRandomModels(TestModel2.class, 50));
+
+        final long startTime = System.currentTimeMillis();
+
+        new SynchronizedTransaction(FlowManager.getDatabase(TestDatabase.class)
+            .beginTransactionAsync(new ProcessModelTransaction.Builder<>(
+                new ProcessModelTransaction.ProcessModel<Model>() {
+                    @Override
+                    public void processModel(Model model) {
+                        model.save();
+                    }
+                }).addAll(modelList).processListener(new ProcessModelTransaction.OnModelProcessListener<Model>() {
+                @Override
+                public void onModelProcessed(long current, long total, Model modifiedModel) {
+                    System.out.println("Processing: " + current + " for a total of: " + total);
+                }
+            }).build())
+        ).successCallback(new Transaction.Success() {
+            @Override
+            public void onSuccess(Transaction transaction) {
+                System.out.println("Transaction completed in: " + (System.currentTimeMillis()
+                    - startTime));
+            }
+        }).execute();
+
     }
 
 }
