dcc8765d8b8a243df3d84879645e0e55a5762792
Andrew Grosner <abgrosner@gmail.com>
2017-03-25 16:25:36 -0400

    [database] add typealias on the process function to clean up code.

--- dbflow-kotlinextensions/src/main/java/com/raizlabs/android/dbflow/kotlinextensions/DatabaseExtensions.kt
+++ dbflow-kotlinextensions/src/main/java/com/raizlabs/android/dbflow/kotlinextensions/DatabaseExtensions.kt
@@ -3,58 +3,65 @@ package com.raizlabs.android.dbflow.kotlinextensions
 import android.content.ContentValues
 import com.raizlabs.android.dbflow.config.DatabaseDefinition
 import com.raizlabs.android.dbflow.config.FlowManager.*
-import com.raizlabs.android.dbflow.structure.BaseQueryModel
 import com.raizlabs.android.dbflow.structure.Model
 import com.raizlabs.android.dbflow.structure.ModelAdapter
+import com.raizlabs.android.dbflow.structure.ModelViewAdapter
 import com.raizlabs.android.dbflow.structure.QueryModelAdapter
 import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper
 import com.raizlabs.android.dbflow.structure.database.transaction.FastStoreModelTransaction.*
 import com.raizlabs.android.dbflow.structure.database.transaction.ProcessModelTransaction
+import com.raizlabs.android.dbflow.structure.database.transaction.ProcessModelTransaction.ProcessModel
 import com.raizlabs.android.dbflow.structure.database.transaction.Transaction
 
-/**
- * Easily get access to its [DatabaseDefinition] directly.
- */
-inline fun <reified T : Any> database() = getDatabase(T::class.java)
+typealias ProcessFunction<T> = (T, DatabaseWrapper) -> Unit
 
 /**
  * Easily get access to its [DatabaseDefinition] directly.
  */
-inline fun <reified T : Any> databaseForTable() = getDatabaseForTable(T::class.java)
+inline fun <reified T> database() = getDatabase(T::class.java)
+
+/**
+ * Easily get access to its [DatabaseDefinition] directly.
+ */
+inline fun <reified T> databaseForTable() = getDatabaseForTable(T::class.java)
 
 /**
  * Easily get its table name.
  */
-inline fun <reified T : Any> tableName() = getTableName(T::class.java)
+inline fun <reified T> tableName() = getTableName(T::class.java)
 
 /**
  * Easily get its [ModelAdapter].
  */
-inline fun <reified T : Any> modelAdapter() = getModelAdapter(T::class.java)
+inline fun <reified T> modelAdapter() = getModelAdapter(T::class.java)
 
 /**
  * Easily get its [QueryModelAdapter].
  */
-inline fun <reified T : BaseQueryModel> queryModelAdapter() = getQueryModelAdapter(T::class.java)
+inline fun <reified T> queryModelAdapter() = getQueryModelAdapter(T::class.java)
+
+/**
+ * Easily get its [ModelViewAdapter]
+ */
+inline fun <reified T> modelViewAdapter() = getModelViewAdapter(T::class.java)
 
 /**
  * Enables a collection of T objects to easily operate on them within a synchronous database transaction.
  */
 inline fun <reified T : Any> Collection<T>.processInTransaction(
-        crossinline processFunction: (T, DatabaseWrapper) -> Unit) {
-    databaseForTable<T>().executeTransaction { db ->
-        forEach { processFunction(it, db) }
-    }
+        crossinline processFunction: ProcessFunction<T>) {
+    databaseForTable<T>().executeTransaction { db -> forEach { processFunction(it, db) } }
 }
 
 /**
  * Places the [Collection] of items on the [ITransactionQueue]. Use the [processFunction] to perform
  * an action on each individual [Model]. This happens on a non-UI thread.
  */
-inline fun <reified T : Any> Collection<T>.processInTransactionAsync(crossinline processFunction: (T, DatabaseWrapper) -> Unit,
-                                                                     success: Transaction.Success? = null,
-                                                                     error: Transaction.Error? = null,
-                                                                     processListener: ProcessModelTransaction.OnModelProcessListener<T>? = null) {
+inline fun <reified T : Any> Collection<T>.processInTransactionAsync(
+        crossinline processFunction: ProcessFunction<T>,
+        success: Transaction.Success? = null,
+        error: Transaction.Error? = null,
+        processListener: ProcessModelTransaction.OnModelProcessListener<T>? = null) {
     databaseForTable<T>().beginTransactionAsync(
             this.processTransaction(processFunction)
                     .processListener(processListener).build())
@@ -62,8 +69,8 @@ inline fun <reified T : Any> Collection<T>.processInTransactionAsync(crossinline
 }
 
 inline fun <reified T : Any> Collection<T>.processTransaction(
-        crossinline processFunction: (T, DatabaseWrapper) -> Unit): ProcessModelTransaction.Builder<T> {
-    return ProcessModelTransaction.Builder<T>(ProcessModelTransaction.ProcessModel { model, wrapper -> processFunction(model, wrapper) }).addAll(this)
+        crossinline processFunction: ProcessFunction<T>): ProcessModelTransaction.Builder<T> {
+    return ProcessModelTransaction.Builder<T>(ProcessModel { model, wrapper -> processFunction(model, wrapper) }).addAll(this)
 }
 
 inline fun <reified T : Any> Collection<T>.fastSave() = saveBuilder(modelAdapter<T>()).addAll(this)
