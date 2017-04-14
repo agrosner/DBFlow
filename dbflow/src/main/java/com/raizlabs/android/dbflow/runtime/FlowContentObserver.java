9b505fbf546bc2cfa8a93d683c943ddc643144fb
Vitaliy <ultraon@mail.ru>
2017-04-15 00:44:37 +0300

    Added important comments to the JavaDoc
    Added important comments to the JavaDoc after investigation thread issue.

--- dbflow/src/main/java/com/raizlabs/android/dbflow/runtime/FlowContentObserver.java
+++ dbflow/src/main/java/com/raizlabs/android/dbflow/runtime/FlowContentObserver.java
@@ -71,7 +71,7 @@ public class FlowContentObserver extends ContentObserver {
 
         /**
          * Notifies that the state of a {@link Model}
-         * has changed for the table this is registered for.
+         * has changed for the table this is registered for. This method can be called from the thread another then main thread.
          *
          * @param table            The table that this change occurred on. This is ONLY available on {@link VERSION_CODES#JELLY_BEAN}
          *                         and up.
@@ -89,7 +89,7 @@ public class FlowContentObserver extends ContentObserver {
     public interface OnTableChangedListener {
 
         /**
-         * Called when table changes.
+         * Called when table changes. This method can be called from the thread another then main thread.
          *
          * @param tableChanged The table that has changed. NULL unless version of app is {@link VERSION_CODES#JELLY_BEAN}
          *                     or higher.
