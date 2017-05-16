8d4cd8ea8c10eb0a2d258d3bb7153896870c16f1
Andrew Grosner <abgrosner@gmail.com>
2017-05-16 10:31:06 -0700

    [adapter] supress nullability issues.

--- dbflow/src/main/java/com/raizlabs/android/dbflow/structure/InternalAdapter.java
+++ dbflow/src/main/java/com/raizlabs/android/dbflow/structure/InternalAdapter.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 /**
  * Description: Used for our internal Adapter classes such as generated {@link ModelAdapter}.
  */
+@SuppressWarnings("NullableProblems")
 public interface InternalAdapter<TModel> {
 
     /**
