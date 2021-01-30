2fba0be80f52f88d7f6d430d29bf89c479a62da9
Andrew Grosner <abgrosner@gmail.com>
2021-01-30 13:31:08 -0500

    [Select] add inline class, update kotlin version.

--- tests/src/androidTest/java/com/dbflow5/DBFlowInstrumentedTestRule.kt
+++ tests/src/androidTest/java/com/dbflow5/DBFlowInstrumentedTestRule.kt
@@ -10,6 +10,7 @@ import com.dbflow5.prepackaged.PrepackagedDB
 import com.dbflow5.provider.ContentDatabase
 import com.dbflow5.runtime.ContentResolverNotifier
 import com.dbflow5.sqlcipher.CipherDatabase
+import com.dbflow5.sqlcipher.SQLCipherOpenHelperImpl
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
@@ -24,28 +25,28 @@ class DBFlowInstrumentedTestRule : TestRule {
             override fun evaluate() {
                 FlowLog.setMinimumLoggingLevel(FlowLog.Level.V)
                 FlowManager.init(FlowConfig.Builder(DemoApp.context)
-                    .database(DatabaseConfig(
-                        databaseClass = AppDatabase::class.java,
-                        openHelperCreator = AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context),
-                        modelNotifier = ContentResolverNotifier(DemoApp.context, "com.grosner.content"),
-                        transactionManagerCreator = { databaseDefinition: DBFlowDatabase ->
-                            ImmediateTransactionManager(databaseDefinition)
-                        }))
-                    .database(DatabaseConfig(
-                        openHelperCreator = AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context),
-                        databaseClass = PrepackagedDB::class.java,
-                        databaseName = "prepackaged"))
-                    .database(DatabaseConfig(
-                        openHelperCreator = AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context),
-                        databaseClass = CipherDatabase::class.java))
-                    .database(DatabaseConfig.builder(ContentDatabase::class,
-                        AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context))
-                        .databaseName("content")
+                        .database(DatabaseConfig(
+                                databaseClass = AppDatabase::class.java,
+                                openHelperCreator = AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context),
+                                modelNotifier = ContentResolverNotifier(DemoApp.context, "com.grosner.content"),
+                                transactionManagerCreator = { databaseDefinition: DBFlowDatabase ->
+                                    ImmediateTransactionManager(databaseDefinition)
+                                }))
+                        .database(DatabaseConfig(
+                                openHelperCreator = AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context),
+                                databaseClass = PrepackagedDB::class.java,
+                                databaseName = "prepackaged"))
+                        .database(DatabaseConfig.builder(CipherDatabase::class) { db, callback ->
+                            SQLCipherOpenHelperImpl(DemoApp.context, db, callback)
+                        }.build())
+                        .database(DatabaseConfig.builder(ContentDatabase::class,
+                                AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context))
+                                .databaseName("content")
+                                .build())
+                        .database(DatabaseConfig.Builder(TestDatabase::class, AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context))
+                                .transactionManagerCreator(::ImmediateTransactionManager)
+                                .build())
                         .build())
-                    .database(DatabaseConfig.Builder(TestDatabase::class, AndroidSQLiteOpenHelper.createHelperCreator(DemoApp.context))
-                        .transactionManagerCreator(::ImmediateTransactionManager)
-                        .build())
-                    .build())
                 try {
                     base.evaluate()
                 } finally {
