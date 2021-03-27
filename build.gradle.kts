3add26ed4eb36ca9cf82f684590642bf4a669667
Andrew Grosner <abgrosner@gmail.com>
2021-03-27 09:50:29 -0400

    [Android] use latest AGP 4.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.android.tools.build:gradle:4.1.3")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
         classpath("com.getkeepsafe.dexcount:dexcount-gradle-plugin:2.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.Kotlin}")
