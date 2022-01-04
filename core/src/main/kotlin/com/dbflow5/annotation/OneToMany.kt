d85761c41fa20dc5c49169ff97ac6739ee5a1c63
fuzzagrosner <andrew@fuzzproductions.com>
2022-01-03 23:08:55 -0500

    [API] description clean

--- core/src/main/kotlin/com/dbflow5/annotation/OneToMany.kt
+++ core/src/main/kotlin/com/dbflow5/annotation/OneToMany.kt
@@ -5,25 +5,29 @@ package com.dbflow5.annotation
  * This annotation can handle loading, deleting, and saving when the current data changes. By default it will call the
  * associated method when the containing class operates.
  */
-@Target(AnnotationTarget.FUNCTION,
-        AnnotationTarget.PROPERTY_GETTER,
-        AnnotationTarget.PROPERTY_SETTER)
+@DBFlowKAPTOnly
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
 @Retention(AnnotationRetention.SOURCE)
-@Deprecated("Use The new OneToManyRelation annotation")
+@Deprecated("Use The new @OneToManyRelation annotation")
 annotation class OneToMany(
-        /**
-         * @return The methods you wish to call it from. By default it's loaded out of the DB.
-         */
-        val oneToManyMethods: Array<OneToManyMethod> = [(OneToManyMethod.LOAD)],
-        /**
-         * @return The name of the list variable to use. If is left blank, we will remove the "get" and then decapitalize the remaining name.
-         */
-        val variableName: String = "",
-        /**
-         * @return If true, the code generated for this relationship done as efficiently as possible.
-         * It will not work on nested relationships, caching, and other code that requires overriding of BaseModel or Model operations.
-         */
-        val efficientMethods: Boolean = true)
+    /**
+     * @return The methods you wish to call it from. By default it's loaded out of the DB.
+     */
+    val oneToManyMethods: Array<OneToManyMethod> = [(OneToManyMethod.LOAD)],
+    /**
+     * @return The name of the list variable to use. If is left blank, we will remove the "get" and then decapitalize the remaining name.
+     */
+    val variableName: String = "",
+    /**
+     * @return If true, the code generated for this relationship done as efficiently as possible.
+     * It will not work on nested relationships, caching, and other code that requires overriding of BaseModel or Model operations.
+     */
+    val efficientMethods: Boolean = true
+)
 
 /**
  * The method to apply the OneToMany to.
