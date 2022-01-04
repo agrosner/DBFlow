36a3affa4be2355acb997c6ca632bd9f754bcf6b
fuzzagrosner <andrew@fuzzproductions.com>
2022-01-03 22:34:10 -0500

    [API] deprecate inherited functionality.

--- core/src/main/kotlin/com/dbflow5/annotation/InheritedPrimaryKey.kt
+++ core/src/main/kotlin/com/dbflow5/annotation/InheritedPrimaryKey.kt
@@ -5,17 +5,22 @@ package com.dbflow5.annotation
  */
 @Target(AnnotationTarget.ANNOTATION_CLASS)
 @Retention(AnnotationRetention.SOURCE)
+@Deprecated(
+    "Inherited non-declared keys are no longer supported. Redeclare the " +
+        "fields in the model class."
+)
 annotation class InheritedPrimaryKey(
-        /**
-         * @return The primary key annotation as if it was part of the class
-         */
-        val primaryKey: PrimaryKey,
-        /**
-         * @return The column annotation as if it was part of the class
-         */
-        val column: Column,
-        /**
-         * @return The field name that an inherited column uses. It must match exactly case-by-case to the field you're referencing.
-         * If the field is private, the [PrimaryKey] allows you to define getter and setters for it.
-         */
-        val fieldName: String)
+    /**
+     * @return The primary key annotation as if it was part of the class
+     */
+    val primaryKey: PrimaryKey,
+    /**
+     * @return The column annotation as if it was part of the class
+     */
+    val column: Column,
+    /**
+     * @return The field name that an inherited column uses. It must match exactly case-by-case to the field you're referencing.
+     * If the field is private, the [PrimaryKey] allows you to define getter and setters for it.
+     */
+    val fieldName: String
+)
