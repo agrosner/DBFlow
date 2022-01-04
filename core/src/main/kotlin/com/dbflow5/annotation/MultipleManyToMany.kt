464bc25fbaa25462d8c0ea33b156c5b04b72035f
fuzzagrosner <andrew@fuzzproductions.com>
2022-01-03 23:07:35 -0500

    [API] cleanup usage.

--- core/src/main/kotlin/com/dbflow5/annotation/MultipleManyToMany.kt
+++ core/src/main/kotlin/com/dbflow5/annotation/MultipleManyToMany.kt
@@ -4,5 +4,5 @@ package com.dbflow5.annotation
  * Description: Provides ability to add multiple [ManyToMany] annotations at once.
  */
 @Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
+@Target(AnnotationTarget.CLASS)
 annotation class MultipleManyToMany(vararg val value: ManyToMany)
