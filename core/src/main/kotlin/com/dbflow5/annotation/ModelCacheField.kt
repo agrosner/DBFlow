243ce8a14eac3a56e6f84dee7b89c9a4aa247695
fuzzagrosner <andrew@fuzzproductions.com>
2022-01-03 22:41:38 -0500

    [API] remove floating annotations.

--- core/src/main/kotlin/com/dbflow5/annotation/ModelCacheField.kt
+++
@@ -1,8 +0,0 @@
-package com.dbflow5.annotation
-
-/**
- * Description: marks a single field as a ModelCache creator that is used in the corresponding ModelAdapter.
- */
-@Target(AnnotationTarget.FIELD)
-@Retention(AnnotationRetention.SOURCE)
-annotation class ModelCacheField
