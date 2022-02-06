2d92bf5a9c1d5d3d34d173ad42753c55769a7e99
fuzzagrosner <andrew@fuzzproductions.com>
2022-02-05 22:07:51 -0500

    [SQL] basic delete implementation.

---
+++ lib/src/main/kotlin/com/dbflow5/query2/Delete.kt
@@ -0,0 +1,21 @@
+package com.dbflow5.query2
+
+import com.dbflow5.adapter.SQLObjectAdapter
+import com.dbflow5.sql.Query
+
+interface Delete<Table : Any> : Query,
+    HasAdapter<Table, SQLObjectAdapter<Table>> {
+}
+
+fun <Table : Any> SQLObjectAdapter<Table>.delete(): Delete<Table> = DeleteImpl(adapter = this)
+
+internal class DeleteImpl<Table : Any>(
+    override val adapter: SQLObjectAdapter<Table>
+) : Delete<Table> {
+
+    override val query: String by lazy {
+        buildString {
+            append("DELETE FROM ${adapter.name} ")
+        }
+    }
+}
