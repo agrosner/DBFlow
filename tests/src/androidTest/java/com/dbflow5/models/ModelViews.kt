2ad386265b3578e418daad2835cb434754e5bda1
fuzzagrosner <andrew@fuzzproductions.com>
2022-02-07 11:39:09 -0500

    [SQL] replace model views with query.

--- tests/src/androidTest/java/com/dbflow5/models/ModelViews.kt
+++ tests/src/androidTest/java/com/dbflow5/models/ModelViews.kt
@@ -6,10 +6,9 @@ import com.dbflow5.annotation.ColumnMap
 import com.dbflow5.annotation.ModelView
 import com.dbflow5.annotation.ModelViewQuery
 import com.dbflow5.config.database
-import com.dbflow5.query.From
 import com.dbflow5.query.property.IProperty
 import com.dbflow5.query.property.property
-import com.dbflow5.query.select
+import com.dbflow5.query2.select
 
 class AuthorName(var name: String = "", var age: Int = 0)
 
@@ -24,14 +23,13 @@ class AuthorView(
         // TODO: switch back to method
         @JvmStatic
         @get:ModelViewQuery
-        val query: From<Author>
-            get() = (select(
+        val query
+            get() = (database<TestDatabase>().authorAdapter.select(
                 Author_Table.id.`as`("authorId"),
                 Author_Table.first_name.concatenate(" ".property as IProperty<out IProperty<*>>)
                     .concatenate(Author_Table.last_name as IProperty<out IProperty<*>>)
                     .`as`("authorName")
-            )
-                from database<TestDatabase>().authorAdapter)
+            ))
     }
 }
 
@@ -41,9 +39,9 @@ class PriorityView(var name: String = "") {
     companion object {
         @JvmStatic
         @get:ModelViewQuery
-        val query: From<Author>
-            get() = select(
+        val query
+            get() = database<TestDatabase>().authorAdapter.select(
                 (Author_Table.first_name + Author_Table.last_name).`as`("name")
-            ) from database<TestDatabase>().authorAdapter
+            )
     }
 }
