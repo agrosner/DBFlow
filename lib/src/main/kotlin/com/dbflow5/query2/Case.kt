abb678b295ef850ada892322ce52606f1070c0ac
fuzzagrosner <andrew@fuzzproductions.com>
2022-02-06 19:15:13 -0500

    [SQL] case operator.

---
+++ lib/src/main/kotlin/com/dbflow5/query2/Case.kt
@@ -0,0 +1,181 @@
+package com.dbflow5.query2
+
+import com.dbflow5.query.BaseOperator
+import com.dbflow5.query.Operator
+import com.dbflow5.query.SQLOperator
+import com.dbflow5.query.property.IProperty
+import com.dbflow5.query.property.Property
+import com.dbflow5.query.property.property
+import com.dbflow5.quoteIfNeeded
+import com.dbflow5.sql.Query
+
+data class CaseCondition<T> internal constructor(
+    val operator: SQLOperator? = null,
+    val value: T? = null,
+    /**
+     * If true, value was used instead of Operator.
+     */
+    val valueSet: Boolean = false,
+    val then: T?,
+)
+
+/**
+ * Represents a completed case since they have an "end" operator.
+ */
+interface CaseCompleted<T> : Query
+
+interface CaseWhenEnabled<T> {
+    fun whenever(
+        operator: SQLOperator,
+        then: T?
+    ): CaseWithWhen<T>
+
+}
+
+interface CaseEfficientWhenEnabled<T> {
+    fun whenever(
+        value: T?,
+        then: T?
+    ): CaseWithWhen<T>
+}
+
+
+interface CaseElseEnabled<T> {
+    infix fun `else`(elseValue: T?): CaseWithElse<T>
+}
+
+interface CaseEndEnabled<T> {
+    infix fun end(columnName: String): Property<CaseCompleted<T>>
+    fun end(): Property<CaseCompleted<T>>
+
+    fun endAsOperator(): Operator<T>
+}
+
+/**
+ * Description: Used in [Select] queries as a parameter.
+ * Represents SQLITE CASE.
+ */
+interface CaseStart<T> : Query,
+    CaseWhenEnabled<T> {
+    val column: IProperty<*>?
+}
+
+interface CaseWithWhen<T> : Query,
+    CaseWhenEnabled<T>,
+    CaseElseEnabled<T>,
+    CaseEndEnabled<T> {
+    val whenStatements: List<CaseCondition<T>>
+}
+
+interface CaseWithElse<T> :
+    CaseEndEnabled<T> {
+    val elseValue: T?
+}
+
+/**
+ * Efficient case method that skips to ELSE.
+ */
+fun <T> case(column: Property<T>): CaseEfficientWhenEnabled<T> = CaseImpl(
+    column = column,
+)
+
+fun <T> case(): CaseStart<T> = CaseImpl()
+
+internal data class CaseImpl<T>(
+    override val column: IProperty<*>? = null,
+    override val whenStatements: List<CaseCondition<T>> = listOf(),
+    override val elseValue: T? = null,
+    private val elseSpecified: Boolean = false,
+    private val endColumnName: String? = null,
+) : Query,
+    CaseStart<T>,
+    CaseWithWhen<T>,
+    CaseEfficientWhenEnabled<T>,
+    CaseWithElse<T>,
+    CaseCompleted<T> {
+
+    /**
+     * If we use a column directly, we do not allow [whenever]
+     * operations.
+     */
+    private val efficientCase = column != null
+
+    override val query: String = buildString {
+        append("CASE ")
+        if (efficientCase) {
+            append(
+                "${
+                    BaseOperator.convertValueToString(
+                        column,
+                        appendInnerQueryParenthesis = false
+                    )
+                } "
+            )
+        }
+        append(whenStatements.joinToString(separator = " ") { condition ->
+            buildString {
+                append("WHEN ")
+                if (efficientCase) {
+                    append(
+                        BaseOperator.convertValueToString(
+                            condition.value,
+                            appendInnerQueryParenthesis = false
+                        )
+                    )
+                } else {
+                    condition.operator?.appendConditionToQuery(this)
+                }
+                append(
+                    " THEN ${
+                        BaseOperator.convertValueToString(
+                            condition.then,
+                            appendInnerQueryParenthesis = false,
+                        )
+                    }"
+                )
+            }
+        })
+
+        if (elseSpecified) {
+            append(
+                " ELSE ${
+                    BaseOperator.convertValueToString(
+                        elseValue,
+                        appendInnerQueryParenthesis = false
+                    )
+                }"
+            )
+        }
+        append(" END ${endColumnName ?: ""}")
+    }
+
+    override fun whenever(operator: SQLOperator, then: T?): CaseWithWhen<T> =
+        copy(
+            whenStatements = whenStatements.toMutableList().apply {
+                add(CaseCondition(operator, then = then))
+            }
+        )
+
+    override fun whenever(value: T?, then: T?): CaseWithWhen<T> =
+        copy(
+            whenStatements = whenStatements.toMutableList().apply {
+                add(CaseCondition(value = value, valueSet = true, then = then))
+            }
+        )
+
+    override fun `else`(elseValue: T?): CaseWithElse<T> =
+        copy(
+            elseValue = elseValue,
+            elseSpecified = true,
+        )
+
+    override fun end(columnName: String): Property<CaseCompleted<T>> =
+        copy(
+            endColumnName = columnName.quoteIfNeeded(),
+        ).property
+
+    override fun end(): Property<CaseCompleted<T>> = property
+
+    override fun endAsOperator(): Operator<T> =
+        Operator.op(property.nameAlias)
+}
--- tests/src/androidTest/java/com/dbflow5/sql/language/CaseTest.kt
+++ tests/src/androidTest/java/com/dbflow5/sql/language/CaseTest.kt
@@ -2,32 +2,42 @@ package com.dbflow5.sql.language
 
 import com.dbflow5.BaseUnitTest
 import com.dbflow5.models.SimpleModel_Table
-import com.dbflow5.query.case
-import com.dbflow5.query.caseWhen
 import com.dbflow5.query.property.propertyString
-import org.junit.Assert.*
+import com.dbflow5.query2.case
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class CaseTest : BaseUnitTest() {
 
     @Test
     fun simpleCaseTest() {
         val case = case(propertyString<String>("country"))
-                .whenever("USA")
-                .then("Domestic")
-                .`else`("Foreign")
-        assertEquals("CASE country WHEN 'USA' THEN 'Domestic' ELSE 'Foreign' END `Country`",
-                case.end("Country").query.trim())
-        assertTrue(case.isEfficientCase)
+            .whenever("USA", then = "Domestic")
+            .`else`("Foreign")
+            .end("Country")
+        assertEquals(
+            "CASE country " +
+                "WHEN 'USA' " +
+                "THEN 'Domestic' " +
+                "ELSE 'Foreign' END `Country`",
+            case.query.trim()
+        )
     }
 
     @Test
     fun searchedCaseTest() {
-        val case = caseWhen<String>(SimpleModel_Table.name.eq("USA")).then("Domestic")
-                .whenever(SimpleModel_Table.name.eq("CA")).then("Canada")
-                .`else`("Foreign")
-        assertEquals("CASE WHEN `name`='USA' THEN 'Domestic' WHEN `name`='CA' THEN 'Canada' ELSE 'Foreign'",
-                case.query.trim())
-        assertFalse(case.isEfficientCase)
+        val case = case<String>()
+            .whenever(SimpleModel_Table.name.eq("USA"), then = "Domestic")
+            .whenever(SimpleModel_Table.name.eq("CA"), then = "Canada")
+            .`else`("Foreign")
+            .end()
+        assertEquals(
+            "CASE " +
+                "WHEN `name`='USA' THEN 'Domestic' " +
+                "WHEN `name`='CA' THEN 'Canada' " +
+                "ELSE 'Foreign' " +
+                "END",
+            case.query.trim()
+        )
     }
 }
