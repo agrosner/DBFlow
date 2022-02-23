97b3e3d62ed6d17c9ca851e4ec23ca06bd19a7d3
fuzzagrosner <andrew@fuzzproductions.com>
2022-02-23 08:43:58 -0500

    [Setup] 7.1.1 of gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("com.android.tools.build:gradle:7.1.1")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
         classpath("com.getkeepsafe.dexcount:dexcount-gradle-plugin:3.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.Kotlin}")
