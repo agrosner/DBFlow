92cfc4d126ffb96d7276afe412e0600dde5f2674
fuzzagrosner <andrew@fuzzproductions.com>
2022-02-28 16:59:51 -0500

    [Lib] make DelicateDBFlowApi.kt a warning.

--- core/src/main/kotlin/com/dbflow5/annotation/opts/DelicateDBFlowApi.kt
+++ core/src/main/kotlin/com/dbflow5/annotation/opts/DelicateDBFlowApi.kt
@@ -6,7 +6,8 @@ package com.dbflow5.annotation.opts
  * These pieces of API may change, but could be used outside of the library.
  */
 @RequiresOptIn(
-    message = "This API is more advanced and can be prone to error."
+    message = "This API is more advanced and can be prone to error.",
+    level = RequiresOptIn.Level.WARNING,
 )
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
